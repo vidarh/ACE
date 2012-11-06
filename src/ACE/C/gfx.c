vidarh/ACE

commit de330d2006007497709e441c8a829919a974969e

Pushed more gen() calls into codegen.c

src/ACE/C/gfx.c
@@ -104,8 +104,7 @@ BOOL colorset=FALSE;
      /* plot point */
      if (!colorset)
      {
-	   gen_load32a("_GfxBase",6);
-	   enter_XREF("_GfxBase");
+	   gen_libbase("Gfx");
 	   gen_load32a("_RPort",1);
 	   enter_XREF("_RPort");
      }
@@ -114,36 +113,30 @@ BOOL colorset=FALSE;
      {
        gen("add.w","36(a1)","d0");   /* x + RPort->cp_x */
        gen("add.w","38(a1)","d1");   /* y + RPort->cp_y */
-       gen_jsr("_LVOMove(a6)");
-       enter_XREF("_LVOMove");
+       gen_libcall("Move","Gfx");
      }
 
      /* 
      ** Save appropriate registers for call to WritePixel()
      ** since Move() may clobber them.
      */ 
-     gen("move.l","a1","a3");
-     gen("move.w","d0","d3");
-     gen("move.w","d1","d4");
-
-     gen_jsr("_LVOMove(a6)");
+     gen_move32aa(1,3);
+     gen_move16dd(0,3);
+     gen_move16dd(1,4);
+     gen_libcall("Move","Gfx");
 
      /* 
      ** Restore appropriate registers for call to WritePixel().
      */
-     gen("move.w","d4","d1");	
-     gen("move.w","d3","d0");	
-     gen("move.l","a3","a1");
-     	
-     gen_jsr("_LVOWritePixel(a6)");
-
-     enter_XREF("_LVOMove");  /* Must call Move() to change pen pos'n */
-     enter_XREF("_LVOWritePixel");  
+     gen_move16dd(4,1);
+     gen_move16dd(3,0);
+     gen_move32aa(3,1);
+     gen_libcall("WritePixel","Gfx");
 
      if (colorset)
      {
 	   /* change back to old pen */
-	   gen("move.w","_fgdpen","d0");
+	   gen_load16d("_fgdpen",0);
 	   gen_gfxcall("SetAPen");
 	   enter_XREF("_fgdpen");
      }
@@ -176,6 +169,7 @@ BOOL bordercolor=FALSE;
     if (sym == comma) 
     {
      insymbol();
+
      if (sym != comma)
      {
       make_sure_short(expr()); /* paintcolor-id */
@@ -314,30 +308,30 @@ BOOL aspect=FALSE;
      {
        gen_load32a("_RPort",1);
 
-       gen("move.w","_shortx","d0");
+       gen_load16d("_shortx",0);
        gen("add.w","36(a1)","d0");   /* x + RPort->cp_x */
        gen("move.w","d0","_shortx");
 
-       gen("move.w","_shorty","d0");
+       gen_load16d("_shorty",0);
        gen("add.w","38(a1)","d0");   /* y + RPort->cp_y */
        gen("move.w","d0","_shorty");
      }
 
      /* convert x & y values to floats */
-     gen("move.w","_shortx","d0");
+     gen_load16d("_shortx",0);
      gen("ext.l","d0","  ");
-     gen_load32a("_MathBase",6);
-     gen_jsr("_LVOSPFlt(a6)");
+     gen_libbase("Math");
+     gen_libcall("SPFlt","Math");
      gen("move.l","d0","_floatx");
 
-     gen("move.w","_shorty","d0");
+     gen_load16d("_shorty",0);
      gen("ext.l","d0","  ");
-     gen_load32a("_MathBase",6);
-     gen_jsr("_LVOSPFlt(a6)");
+     gen_libbase("Math");
+     gen_libcall("SPFlt","Math");
      gen("move.l","d0","_floaty");
 
-     gen("move.l","_floatx","d0");
-     gen("move.l","_floaty","d1");
+     gen_load32d("_floatx",0);
+     gen_load32d("_floaty",1);
 
      if (!start_angle) gen("moveq","#0","d3");  /* default is zero */
      if (!end_angle)  gen("move.l","#$b3800049","d4");  /* default is 359 */
@@ -346,7 +340,6 @@ BOOL aspect=FALSE;
      gen_jsr("_ellipse");
      enter_XREF("_ellipse");
      enter_XREF("_GfxBase");
-     enter_XREF("_MathBase");
      enter_XREF("_MathTransBase");  /* need these 3 libs for _ellipse */
 
      enter_BSS("_shortx","ds.w 1");
@@ -357,10 +350,8 @@ BOOL aspect=FALSE;
      if (colorset)
      {
       /* change back to old pen */
-      gen("move.w","_fgdpen","d0");
-      gen_load32a("_RPort",1);
-      gen_load32a("_RPort",6);
-      gen_jsr("_LVOSetAPen(a6)");
+      gen_load16d("_fgdpen",0);
+      gen_gfxcall("SetAPen");
       enter_XREF("_fgdpen");
      }          
 }
@@ -485,70 +476,64 @@ CODE *cx,*cx1,*cx2,*cx3,*cx4,*cx5,*cx6;
         }
       }
 
-      /* draw the line, outline box, or filled box */
-      if (box)
-      {
-	gen_pop16d(5);  /* y2 */
-	gen_pop16d(4);  /* x2 */
-	gen("move.w","_ymin","d3");     /* y1 */
-	gen("move.w","_xmin","d2");     /* x1 */
-	/* x1=d2; y1=d3 x2=d4; y2=d5 */
-	/* already moved to x1,y1 */
-
-	gen("move.w","d4","d0");	
-	gen("move.w","d3","d1");	
-	gen_gfxcall("Draw");     /* x1,y1 - x2,y1 */
-	gen("move.w","d4","d0");	
-	gen("move.w","d5","d1");	
-	gen_jsr("_LVODraw(a6)"); /* x2,y1 - x2,y2 */
-	gen("move.w","d2","d0");	
-	gen("move.w","d5","d1");	
-	gen_jsr("_LVODraw(a6)"); /* x2,y2 - x1,y2 */
-	gen("move.w","d2","d0");	
-	gen("move.w","d3","d1");	
-	gen_jsr("_LVODraw(a6)"); /* x1,y2 - x1,y1 */
-	enter_XREF("_LVODraw");
-	enter_XREF("_LVOMove");
-	enter_BSS("_xmin","ds.w 1");
-	enter_BSS("_ymin","ds.w 1");
-      }		
-      else
-      if (boxfill)
-      {
-	change(cx,"nop","  ","  ");   /* don't need Move */
-	gen_load32a("_RPort",1);
-	gen_load32a("_GfxBase",6);
-	gen_pop16d(3);  /* ymax */
-	gen_pop16d(2);  /* xmax */
-	gen("move.w","_ymin","d1");     /* ymin */
-	gen("move.w","_xmin","d0");     /* xmin */
-	gen_jsr("_LVORectFill(a6)");
-	enter_XREF("_LVORectFill"); 
-	enter_BSS("_xmin","ds.w 1");
-	enter_BSS("_ymin","ds.w 1");
-      }
-      else
-      {
-        /* draw line */
-        gen_load32a("_RPort",1);
-        gen_load32a("_GfxBase",6);
-		gen_pop16d(1);  /* y2 */
-		gen_pop16d(0);  /* x2 */
-		/* already moved to x1,y1 */
-        gen_jsr("_LVODraw(a6)");
-        enter_XREF("_LVODraw");
-		enter_XREF("_LVOMove");
-	/* don't need to save x1 & x2 in _xmin & _ymin */
-	change(cx1,"nop","  ","  ");
- 	change(cx2,"nop","  ","  ");
-	if (colorset)
-	{
-	 change(cx3,"nop","  ","  ");
-	 change(cx4,"nop","  ","  ");
-	 change(cx5,"nop","  ","  ");
-	 change(cx6,"nop","  ","  ");
-	}
-      }
+	   /* draw the line, outline box, or filled box */
+	   if (box)
+		 {
+		   gen_pop16d(5);  /* y2 */
+		   gen_pop16d(4);  /* x2 */
+		   gen_load16d("_ymin",3); /* y1 */
+		   gen_load16d("_xmin",2); /* x1 */
+		   /* x1=d2; y1=d3 x2=d4; y2=d5 */
+		   /* already moved to x1,y1 */
+		   
+		   gen_move16dd(4,0);
+		   gen_move16dd(3,1);
+		   gen_gfxcall("Draw");     /* x1,y1 - x2,y1 */
+		   gen_move16dd(4,0);
+		   gen_move16dd(5,1);
+		   gen_libcall("Draw","Gfx"); /* x2,y1 - x2,y2 */
+		   gen_move16dd(2,0);
+		   gen_move16dd(5,1);
+		   gen_libcall("Draw","Gfx"); /* x2,y2 - x1,y2 */
+		   gen_move16dd(2,0);
+		   gen_move16dd(3,1);
+		   gen_libcall("Draw","Gfx"); /* x1,y2 - x1,y1 */
+		   enter_BSS("_xmin","ds.w 1");
+		   enter_BSS("_ymin","ds.w 1");
+		 }		
+	   else if (boxfill)
+		 {
+		   change(cx,"nop","  ","  ");   /* don't need Move */
+		   gen_load32a("_RPort",1);
+		   gen_libbase("Gfx");
+		   gen_pop16d(3);  /* ymax */
+		   gen_pop16d(2);  /* xmax */
+		   gen_load16d("_ymin",1); /* ymin */
+		   gen_load16d("_xmin",0); /* xmin */
+		   gen_libcall("RectFill","Gfx");
+		   enter_BSS("_xmin","ds.w 1");
+		   enter_BSS("_ymin","ds.w 1");
+		 }
+	   else
+		 {
+		   /* draw line */
+		   gen_load32a("_RPort",1);
+		   gen_libbase("Gfx");
+		   gen_pop16d(1);  /* y2 */
+		   gen_pop16d(0);  /* x2 */
+		   /* already moved to x1,y1 */
+		   gen_libcall("Draw","Gfx");
+		   /* don't need to save x1 & x2 in _xmin & _ymin */
+		   change(cx1,"nop","  ","  ");
+		   change(cx2,"nop","  ","  ");
+		   if (colorset)
+			 {
+			   change(cx3,"nop","  ","  ");
+			   change(cx4,"nop","  ","  ");
+			   change(cx5,"nop","  ","  ");
+			   change(cx6,"nop","  ","  ");
+			 }
+		 }
      }  	    
     }
    }
@@ -584,8 +569,8 @@ void color()
   insymbol();
   make_sure_short(expr());
   gen_pop16d(0);
-  gen("move.w","d0","_bg");  /* background pen for text color change */
-  gen("move.w","d0","_bgpen"); /* change global background pen color */
+  gen_save16d(0,"_bg");  /* background pen for text color change */
+  gen_save16d(0,"_bgpen"); /* change global background pen color */
   gen_gfxcall("SetBPen");
   enter_XREF("_bgpen");
   enter_BSS("_bg","ds.w 1");
@@ -599,8 +584,8 @@ void color()
  }
 
  /* call text color change routine */
- gen("move.w","_fg","d0");
- gen("move.w","_bg","d1");
+ gen_load16d("_fg",0);
+ gen_load16d("_bg",0);
  gen_jsr("_changetextcolor");
  enter_XREF("_changetextcolor");
  enter_XREF("_DOSBase");

src/ACE/C/basfun.c
@@ -651,15 +651,13 @@ BOOL offset_on_stack;
 			   gen_pop_addr(0); /* instr */
 			   make_temp_string();
 			   gen("lea",tempstrname,"a1"); /* outstr */
-			   gen("movea.l","a0","a2");
+			   gen_move32aa(0,2);
 			   gen_jsr("_strlen"); /* inlen in d0 */
 			   sprintf(srcbuf,"#%d",MAXSTRLEN); /* #MAXSTRLEN */
 			   gen("move.l",srcbuf,"d1"); /* outlen = MAXSTRLEN */
-			   gen_load32a("_TransBase",6);
-			   gen_jsr("_LVOTranslate(a6)");
+			   gen_libbase("Trans");
+			   gen_libcall("Translate","Trans");
 			   gen("pea",tempstrname,"  "); /* outstr on stack */
-			   enter_XREF("_TransBase");
-			   enter_XREF("_LVOTranslate");
 			   enter_XREF("_strlen");
 			   sftype=stringtype;
 			  }
@@ -703,28 +701,21 @@ BOOL offset_on_stack;
 }
 
 /* numeric functions */
-int gen_single_func(funcname,nftype)
-char *funcname;
-int  nftype;
+int gen_single_func(char * funcname,int nftype)
 {
-char func[80];
-
+  
   if (nftype != stringtype)
-  {
-   if (nftype != singletype) gen_Flt(nftype);  
-   gen_pop32d(0);
-   gen_load32a("_MathTransBase",6);
-   strcpy(func,funcname);
-   strcat(func,"(a6)");
-   gen_jsr(func);
-   gen_push32d(0);
-   enter_XREF(funcname);
-   enter_XREF("_MathTransBase");
-   enter_XREF("_MathBase");
-   nftype=singletype;
-  }
+	{
+	  if (nftype != singletype) gen_Flt(nftype);  
+	  gen_pop32d(0);
+	  gen_libbase("MathTrans");
+	  gen_libcall(funcname,"MathTrans");
+	  gen_push32d(0);
+	  enter_XREF("_MathBase");
+	  nftype=singletype;
+	}
   else { _error(4); nftype=undefined; }
- return(nftype);
+  return(nftype);
 }
 
 BOOL numfunc()
@@ -860,7 +851,7 @@ char varptr_obj_name[MAXIDSIZE];
 			break;
   	 
 	 /* ATN */
-         case atnsym  : nftype = gen_single_func("_LVOSPAtan",nftype);
+         case atnsym  : nftype = gen_single_func("SPAtan",nftype);
 		        break;
 
 	 /* CINT */
@@ -894,7 +885,7 @@ char varptr_obj_name[MAXIDSIZE];
 			break;
 
 	 /* COS */
-         case cossym  : nftype = gen_single_func("_LVOSPCos",nftype);
+         case cossym  : nftype = gen_single_func("SPCos",nftype);
 		        break;
 
 	 /* CSNG */
@@ -926,18 +917,16 @@ char varptr_obj_name[MAXIDSIZE];
 		  	break;
 
 	 /* EXP */
-         case expsym  : nftype = gen_single_func("_LVOSPExp",nftype);
+         case expsym  : nftype = gen_single_func("SPExp",nftype);
 		        break;
 
 	 /* FIX */
 	 case fixsym  : if (nftype == singletype)
 			{
 			 gen_pop32d(0);
-			 gen_load32a("_MathBase",6);
-			 gen_jsr("_LVOSPFix(a6)");
+			 gen_libbase("Math");
+			 gen_libcall("SPFix","Math");
 			 gen_push32d(0);
-			 enter_XREF("_MathBase");
-			 enter_XREF("_LVOSPFix");
 			 nftype=longtype;
 			}
 			else
@@ -1019,13 +1008,10 @@ char varptr_obj_name[MAXIDSIZE];
 	 case intsym  : if (nftype == singletype)
 			{
 			 gen_pop32d(0);
-			 gen_load32a("_MathBase",6);
-			 gen_jsr("_LVOSPFloor(a6)");
-			 gen_jsr("_LVOSPFix(a6)");
+			 gen_libbase("Math");
+			 gen_libcall("SPFloor","Math");
+			 gen_libcall("SPFix","Math");
 			 gen_push32d(0);
-			 enter_XREF("_MathBase");
-			 enter_XREF("_LVOSPFloor");
-			 enter_XREF("_LVOSPFix");
 			 nftype=longtype;
 			}
 			else
@@ -1069,7 +1055,7 @@ char varptr_obj_name[MAXIDSIZE];
  			break;
 			   
 	 /* LOG */
-         case logsym  : nftype = gen_single_func("_LVOSPLog",nftype);
+         case logsym  : nftype = gen_single_func("SPLog",nftype);
 		        break;
 
 	 /* LONGINT */
@@ -1228,9 +1214,6 @@ char varptr_obj_name[MAXIDSIZE];
 			  gen_pop16d(0);  /* x */
 			  gen_gfxcall("ReadPixel");
 			  gen_push32d(0);
-			  enter_XREF("_LVOReadPixel");
-			  enter_XREF("_GfxBase");
-			  enter_XREF("_RPort");
 			  nftype=longtype;
 			 }
 			}
@@ -1385,11 +1368,11 @@ char varptr_obj_name[MAXIDSIZE];
 			break;
 			
 	 /* SQR */
-         case sqrsym  : nftype = gen_single_func("_LVOSPSqrt",nftype);
+         case sqrsym  : nftype = gen_single_func("SPSqrt",nftype);
 		        break;
 
 	 /* SIN */
-         case sinsym  : nftype = gen_single_func("_LVOSPSin",nftype);
+         case sinsym  : nftype = gen_single_func("SPSin",nftype);
 		        break;
 
 	 /* SIZEOF */
@@ -1414,7 +1397,7 @@ char varptr_obj_name[MAXIDSIZE];
 			 break;
 
 	 /* TAN */
-         case tansym  : nftype = gen_single_func("_LVOSPTan",nftype);
+         case tansym  : nftype = gen_single_func("SPTan",nftype);
 		        break;
 
 	 /* VARPTR */

src/ACE/C/codegen.c
@@ -180,18 +180,58 @@ void gen_load32d(const char * label, unsigned char reg)
   gen("move.l",label,dreg[reg]);
 }
 
+void gen_load16d(const char * label, unsigned char reg)
+{
+  gen("move.w",label,dreg[reg]);
+}
+
+
+void gen_save16d(unsigned char reg, const char * label)
+{
+  gen("move.w",dreg[reg],label);
+}
+
+void gen_move16dd(unsigned char srcreg, unsigned char destreg)
+{
+  gen("move.w",dreg[srcreg],dreg[destreg]);
+}
+
+void gen_move32dd(unsigned char srcreg, unsigned char destreg)
+{
+  gen("move.l",dreg[srcreg],dreg[destreg]);
+}
+
+void gen_move32aa(unsigned char srcreg, unsigned char destreg)
+{
+  gen("move.l",areg[srcreg],areg[destreg]);
+}
+
 /***** "Mid level" code generation functions *****/
 
-void gen_gfxcall(const char * lvo)
+void gen_libbase(const char * base)
+{
+  char buf[200];
+  strcpy(buf,"_");
+  strncat(buf,base,sizeof(buf)-1);
+  strncat(buf,"Base",sizeof(buf)-strlen(buf));
+  gen_load32a(buf,6);
+  enter_XREF(buf);
+}
+
+void gen_libcall(const char * lvo, const char * base)
 {
   char buf[200];
   strcpy(buf,"_LVO");
   strncat(buf,lvo,sizeof(buf)-4);
   enter_XREF(buf);
   strncat(buf,"(a6)",sizeof(buf)-strlen(buf));
-  gen_load32a("_RPort",1);
-  gen_load32a("_GfxBase",6);
-  enter_XREF("_GfxBase");
-  enter_XREF("_RPort");
   gen_jsr(buf);
+}
+
+void gen_gfxcall(const char * lvo)
+{
+  gen_load32a("_RPort",1);
+  enter_XREF("_RPort");
+  gen_libbase("Gfx");
+  gen_libcall(lvo,"Gfx");
 }

src/ACE/C/codegen.h
@@ -27,7 +27,15 @@ void gen_tst32d(unsigned char reg);
 void gen_tst16d(unsigned char reg);
 void gen_load32a(const char * label, unsigned char reg);
 void gen_load32d(const char * label, unsigned char reg);
+void gen_load16d(const char * label, unsigned char reg);
+void gen_move16dd(unsigned char srcreg, unsigned char destreg);
+void gen_move32dd(unsigned char srcreg, unsigned char destreg);
+void gen_move32aa(unsigned char srcreg, unsigned char destreg);
 
+/* Mid level */
+
+void gen_libbase(const char * base);
+void gen_libcall(const char * lvo, const char * base);
 void gen_gfxcall(const char * lvo);
 
 #endif

src/ACE/C/control.c
@@ -565,16 +565,14 @@ int  countertype,limittype,steptype;
     { 
      gen("moveq","#0","d1");
      gen_load32d(stpbuf,0);   /* d0 < d1? (where d1=0) */
-     gen_load32a("_MathBase",6);
-     gen_jsr("_LVOSPCmp(a6)");
-     enter_XREF("_MathBase");
-     enter_XREF("_LVOSPCmp");
+     gen_libbase("Math");
+     gen_libcall("SPCmp","Math");
      make_label(labname2,lablabel2);
      gen_blt(labname2);  /* test result of ffp Cmp above */
      gen_load32d(cntbuf,0);    /* counter */
      gen_load32d(limbuf,1);   /* limit */
-     gen_load32a("_MathBase",6);
-     gen_jsr("_LVOSPCmp(a6)");
+     gen_libbase("Math");
+     gen_libcall("SPCmp","Math");
      gen_bgt("  ");	  /* if STEP +ve -> counter>limit? */
      cx1=curr_code;
      make_label(labname3,lablabel3); /* don't want to do -ve step test too! */
@@ -582,8 +580,8 @@ int  countertype,limittype,steptype;
      gen_label(lablabel2);
      gen_load32d(cntbuf,0);   /* counter */
      gen_load32d(limbuf,1);   /* limit */
-     gen_load32a("_MathBase",6);
-     gen_jsr("_LVOSPCmp(a6)");
+     gen_libbase("Math");
+     gen_libcall("SPCmp","Math");
      gen_blt("  ");          /* if STEP -ve -> counter<limit? */
      cx2=curr_code;
      gen_label(lablabel3);      /* label for bypassing -ve step test */
@@ -617,12 +615,10 @@ int  countertype,limittype,steptype;
 			break;
 	case singletype :  gen_load32d(stpbuf,0);
 	  gen_load32d(cntbuf,1);
-	  gen_load32a("_MathBase",6);
-	  gen_jsr("_LVOSPAdd(a6)");
-			gen("move.l","d0",counteraddr);
-     			enter_XREF("_MathBase");
-     			enter_XREF("_LVOSPAdd");
-			break;
+	  gen_libbase("Math");
+	  gen_libcall("SPAdd","Math");
+	  gen("move.l","d0",counteraddr);
+	  break;
     }
 
     check_for_event();

src/ACE/C/expr.c
@@ -245,11 +245,9 @@ BOOL negate=FALSE;
    case longtype   : gen("neg.l","(sp)","  "); break;
 
    case singletype : gen_pop32d(0); 
-       gen_load32a("_MathBase",6);
-       gen_jsr("_LVOSPNeg(a6)");
+       gen_libbase("Math");
+       gen_libcall("SPNeg","Math");
        gen_push32d(0);
-       enter_XREF("_MathBase");
-       enter_XREF("_LVOSPNeg");
        break;
    case stringtype : _error(4); break;
   }
@@ -330,23 +328,21 @@ CODE *cx[5];
 					localtype=longtype;
 					break;
 
-		     case singletype :  gen_load32a("_MathBase",6);
-					gen_jsr("_LVOSPMul(a6)");
-					enter_XREF("_MathBase");
-      		     			enter_XREF("_LVOSPMul");
-					localtype=singletype;
-      		     			break;
+			case singletype :  
+			  gen_libbase("Math");
+			  gen_libcall("SPMul","Math");
+			  localtype=singletype;
+			  break;
 		    }
 		    break;
 
-    case fdiv     : gen_pop32d(1);  /* 2nd operand */
-		    gen_pop32d(0);  /* 1st operand */
-		    gen_load32a("_MathBase",6);
-		    gen_jsr("_LVOSPDiv(a6)");  
-		    enter_XREF("_MathBase");
-      		    enter_XREF("_LVOSPDiv");
-		    localtype=singletype;
-      		    break;
+    case fdiv:
+	  gen_pop32d(1);  /* 2nd operand */
+	  gen_pop32d(0);  /* 1st operand */
+	  gen_libbase("Math");
+	  gen_libcall("SPDiv","Math");  
+	  localtype=singletype;
+	  break;
    }
 
    push_result(localtype);  /* result */
@@ -538,11 +534,10 @@ CODE *cx[5];
     case longtype   :	gen("add.l","d1","d0");
     		    	break;
 
-    case singletype : 	gen_load32a("_MathBase",6);
-        		gen_jsr("_LVOSPAdd(a6)");
-        		enter_XREF("_LVOSPAdd");
-        		enter_XREF("_MathBase");
-        		break;
+    case singletype : 	
+	  gen_libbase("Math");
+	  gen_libcall("SPAdd","Math");
+	  break;
 
     case stringtype : 	/* copy source to temp string */
         		gen_pop_addr(2); /* 2nd */
@@ -583,11 +578,10 @@ CODE *cx[5];
     case longtype   : 	gen("sub.l","d1","d0");
          		break;
 
-    case singletype :	gen_load32a("_MathBase",6);
-        		gen_jsr("_LVOSPSub(a6)");
-       			enter_XREF("_LVOSPSub");
-        		enter_XREF("_MathBase");
-        		break;
+    case singletype :	
+	  gen_libbase("Math");
+	  gen_libcall("SPSub","Math");
+	  break;
 
     case stringtype : 	_error(4); break;
   }
@@ -689,14 +683,13 @@ CODE *cx[5];
         		gen("cmp.l","d1","d0");
         		break;
 
-    case singletype : 	gen_pop32d(1);  /* 2nd */
-        		gen_pop32d(0);  /* 1st */
-        		gen("moveq","#-1","d5");     /* assume true */
-        		gen_load32a("_MathBase",6);
-        		gen_jsr("_LVOSPCmp(a6)");
-        		enter_XREF("_LVOSPCmp");
-        		enter_XREF("_MathBase");
-        		break;
+    case singletype : 	
+	  gen_pop32d(1);  /* 2nd */
+	  gen_pop32d(0);  /* 1st */
+	  gen("moveq","#-1","d5");     /* assume true */
+	  gen_libbase("Math");
+	  gen_libcall("SPCmp","Math");
+	  break;
 
     case stringtype : 	gen_pop_addr(1);  /* addr of 2nd string */
 			gen_pop_addr(0);  /* addr of 1st string */
@@ -1043,11 +1036,9 @@ int typ;
 
   if (typ == shorttype) gen("ext.l","d0","  "); /* extend sign */
 
-  gen_load32a("_MathBase",6);
-  gen_jsr("_LVOSPFlt(a6)");
+  gen_libbase("Math");
+  gen_libcall("SPFlt","Math");
   gen_push32d(0);
-  enter_XREF("_LVOSPFlt");
-  enter_XREF("_MathBase");
 }
 
 void change_Flt(exptyp,cx)

src/ACE/C/statement.c
@@ -1178,14 +1178,13 @@ SHORT popcount;
 		     case longtype   : gen("add.l","#1","(a0)");
 		     		       break;
 
-		     case singletype : gen_load32a("_MathBase",6);
-				       gen("move.l","(a0)","d0");
-				       gen("move.l","#$80000041","d1");
-				       gen_jsr("_LVOSPAdd(a6)");
-				       gen("move.l","d0","(a0)");
-				       enter_XREF("_MathBase");
-				       enter_XREF("_LVOSPAdd");
-				       break;
+		     case singletype : 
+			   gen_libbase("Math");
+			   gen("move.l","(a0)","d0");
+			   gen("move.l","#$80000041","d1");
+			   gen_libcall("SPAdd","Math");
+			   gen("move.l","d0","(a0)");
+			   break;
 		    }
  		   }
  		  }
@@ -1227,20 +1226,21 @@ SHORT popcount;
 		    /* increment it by 1 */
 		    switch(dec_item->type)
 		    {
-		     case shorttype  : gen("sub.w","#1","(a0)");
-		     		       break;
+		     case shorttype: 
+			   gen("sub.w","#1","(a0)");
+			   break;
 
-		     case longtype   : gen("sub.l","#1","(a0)");
-		     		       break;
+		     case longtype: 
+			   gen("sub.l","#1","(a0)");
+			   break;
 
-		     case singletype : gen_load32a("_MathBase",6);
-				       gen("move.l","(a0)","d0");
-				       gen("move.l","#$80000041","d1");
-				       gen_jsr("_LVOSPSub(a6)");
-				       gen("move.l","d0","(a0)");
-				       enter_XREF("_MathBase");
-				       enter_XREF("_LVOSPSub");
-				       break;
+		     case singletype : 
+			   gen_libbase("Math");
+			   gen("move.l","(a0)","d0");
+			   gen("move.l","#$80000041","d1");
+			   gen_libcall("SPSub","Math");
+			   gen("move.l","d0","(a0)");
+			   break;
 		    }
  		   }
  		  }

src/ACE/C/sub.c
@@ -265,10 +265,8 @@ int   formaltype[MAXPARAMS];
   {
    /* disable multi-tasking 
       before passing parameters */
-   gen_load32a("_AbsExecBase",6);
-   gen_jsr("_LVOForbid(a6)");
-   enter_XREF("_AbsExecBase");
-   enter_XREF("_LVOForbid");
+   gen_libbase("AbsExec");
+   gen_libcall("Forbid","AbsExec");
 
    /* load parameters into next frame */
    for (n=0;n<sub_ptr->no_of_params;n++)
@@ -302,10 +300,8 @@ char  addrbuf[40];
  else
  {
   /* if actual parameters passed, Forbid() called -> Permit() */
-  gen_load32a("_AbsExecBase",6);
-  gen_jsr("_LVOPermit(a6)");
-  enter_XREF("_AbsExecBase");
-  enter_XREF("_LVOPermit");
+  gen_libbase("AbsExec");
+  gen_libcall("Permit","AbsExec");
 
   /* formal parameters expected */
   do

commit dc7567b68b98c232135a83cddd9d5bfd65f2ac6d

Fixed regression introduced in de330d2

src/ACE/C/gfx.c
@@ -417,7 +417,7 @@ void color() {
   
   /* call text color change routine */
   gen_load16d("_fg",0);
-  gen_load16d("_bg",0);
+  gen_load16d("_bg",1);
   gen_jsr("_changetextcolor");
 }
 