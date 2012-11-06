vidarh/ACE

commit 30c3dcb6483a7835b28a55387534da2a5653874a

Got rid of lea

src/ACE/C/assign.c
@@ -493,7 +493,7 @@ int  exprtype;
 		      {
 			/* string */
 			gen_pop_addr(1);
-			gen("lea",ext_name,"a0");	
+			gen_load_addr(ext_name,0);
 			gen_jsr("_strcpy");
 			enter_XREF("_strcpy");
 		      }	
@@ -791,7 +791,7 @@ SYM  *storage;
    /* ALL data types need a temporary string */
    make_temp_string();
    if (storage->type != stringtype)
-   	gen("lea",tempstrname,"a1");
+   	gen_load_addr(tempstrname,0);
    else
 	gen("pea",tempstrname,"  ");
 

src/ACE/C/basfun.c
@@ -139,7 +139,7 @@ BOOL offset_on_stack;
       			  /* create a string to copy value to */
       			  make_string_store();
       			  strcpy(buf,strstorename);
-      			  gen("lea",buf,"a0");
+      			  gen_load_addr(buf,0);
       			  gen_jsr("_chrstring");
       			  gen("pea",strstorename,"  ");
       			  enter_XREF("_chrstring");
@@ -185,7 +185,7 @@ BOOL offset_on_stack;
 			 if (make_integer(sftype) == shorttype)
 			     make_long(); /* only handle long val */
 			 make_temp_string();
-			 gen("lea",tempstrname,"a0");
+			 gen_load_addr(tempstrname,0);
 			 gen_pop32d(0); /* long argument */
 			 gen_jsr("_binstr");
 			 enter_XREF("_binstr");
@@ -230,7 +230,7 @@ BOOL offset_on_stack;
 			{
 			 sftype = make_integer(sftype);
 			 make_temp_string();
-			 gen("lea",tempstrname,"a0");
+			 gen_load_addr(tempstrname,0);
 			 if (sftype == longtype)
 			 {
 			  gen_pop32d(0);
@@ -419,7 +419,7 @@ BOOL offset_on_stack;
 			  gen_pop16d(0);  /* index */
 			  gen_pop_addr(0);  /* string */
 			  make_temp_string();
-			  gen("lea",tempstrname,"a1");
+			  gen_load_addr(tempstrname,1);
 			  gen_jsr("_leftstr");
 			  gen_push_addr(0);  /* addr of left$ */
 			  enter_XREF("_leftstr");
@@ -448,7 +448,7 @@ BOOL offset_on_stack;
 			 if (make_integer(sftype) == shorttype)
 			     make_long(); /* only handle long val */
 			 make_temp_string();
-			 gen("lea",tempstrname,"a0");
+			 gen_load_addr(tempstrname,0);
 			 gen_pop32d(0); /* long argument */
 			 gen_jsr("_octstr");
 			 enter_XREF("_octstr");
@@ -468,7 +468,7 @@ BOOL offset_on_stack;
 			  gen_pop16d(0);  /* index */
 			  gen_pop_addr(0);  /* string */
 			  make_temp_string();
-			  gen("lea",tempstrname,"a1");
+			  gen_load_addr(tempstrname,1);
 		   	  gen_jsr("_rightstr");
 			  gen_push_addr(0);  /* addr of right$ */
 			  enter_XREF("_rightstr");
@@ -492,7 +492,7 @@ BOOL offset_on_stack;
 			 make_sure_short(sftype);
 			 gen_pop16d(0);
 			 make_temp_string();
-			 gen("lea",tempstrname,"a0");
+			 gen_load_addr(tempstrname,0);
 			 if (func == spacestrsym)
 			   	gen_jsr("_spacestring");
 			 else
@@ -511,7 +511,7 @@ BOOL offset_on_stack;
     case strstrsym :	if (sftype != stringtype)
 			{
 			 make_temp_string();
-			 gen("lea",tempstrname,"a0");
+			 gen_load_addr(tempstrname,0);
 			 if (sftype == longtype)
 			 {
 			  gen_pop32d(0);
@@ -570,7 +570,7 @@ BOOL offset_on_stack;
 
 			  /* call STRING$ */
 			  make_temp_string();
-			  gen("lea",tempstrname,"a0");
+			  gen_load_addr(tempstrname,0);
 			  gen_jsr("_stringstr");
 			  gen_push32d(0);	/* push string result */
 			  enter_XREF("_stringstr");
@@ -605,8 +605,8 @@ BOOL offset_on_stack;
 			   gen_pop16d(0);  /* start posn */
 			   gen_pop_addr(0);  /* string */
 			   make_temp_string();
-			   gen("lea",tempstrname,"a1");
-		           gen_jsr("_midstr");
+			   gen_load_addr(tempstrname,1);
+			   gen_jsr("_midstr");
 			   gen_push_addr(0);  /* addr of mid$ */
 			   enter_XREF("_midstr");
 			   sftype=stringtype;
@@ -650,7 +650,7 @@ BOOL offset_on_stack;
 			  {
 			   gen_pop_addr(0); /* instr */
 			   make_temp_string();
-			   gen("lea",tempstrname,"a1"); /* outstr */
+			   gen_load_addr(tempstrname,1); /* outstr */
 			   gen_move32aa(0,2);
 			   gen_jsr("_strlen"); /* inlen in d0 */
 			   sprintf(srcbuf,"#%d",MAXSTRLEN); /* #MAXSTRLEN */
@@ -669,7 +669,7 @@ BOOL offset_on_stack;
 			{
 			 gen_pop_addr(1);
 		   	 make_temp_string();
-			 gen("lea",tempstrname,"a0"); /* result buffer */
+			 gen_load_addr(tempstrname,0); /* result buffer */
 			 gen_jsr("_ucase");
 			 gen_push_addr(0);
 			 enter_XREF("_ucase");	

src/ACE/C/codegen.c
@@ -165,6 +165,11 @@ void gen_push_addr(unsigned char reg)
   gen("move.l",areg[reg],"-(sp)");
 }
 
+void gen_load_addr(const char * label,unsigned char reg)
+{
+  gen("lea",label,areg[reg]);
+}
+
 void gen_pop_addr(unsigned char reg)
 {
   gen("move.l","(sp)+",areg[reg]);

src/ACE/C/codegen.h
@@ -10,6 +10,7 @@ void gen_pop32d(unsigned char reg);
 void gen_push16d(unsigned char reg);
 void gen_pop16d(unsigned char reg);
 void gen_push_addr(unsigned char reg);
+void gen_load_addr(const char * label,unsigned char reg);
 void gen_pop_addr(unsigned char reg);
 void gen_pop_ignore(unsigned int bytes);
 void gen_pop32_var(const char * label);

src/ACE/C/declare.c
@@ -827,8 +827,8 @@ char bss_size[20];
 				 	{
 			 			/* initialise with the NULL string */
 			 			enter_DATA("_nullstring:","dc.b 0");
-			 			gen("lea",extvarid,"a0");
-			 			gen("lea","_nullstring","a1");
+			 			gen_load_addr(extvarid,0);
+			 			gen_load_addr("_nullstring",1);
 			 			gen_jsr("_strcpy");
 						enter_XREF("_strcpy");
 				 	}

src/ACE/C/expr.c
@@ -543,11 +543,11 @@ CODE *cx[5];
         		gen_pop_addr(2); /* 2nd */
         		gen_pop_addr(1); /* 1st */
 			make_temp_string();
-        		gen("lea",tempstrname,"a0");
+        		gen_load_addr(tempstrname,0);
         		gen_jsr("_strcpy");
         		/* prepare for strcat */
-        		gen("lea",tempstrname,"a0");
-        		gen("move.l","a2","a1");
+        		gen_load_addr(tempstrname,0);
+        		gen_move32aa(2,1);
         		gen_jsr("_strcat");
         		gen("pea",tempstrname,"  ");
         		enter_XREF("_strcpy");

src/ACE/C/file.c
@@ -438,7 +438,7 @@ SYM  *storage;
   
     /* ALL data types need a temporary string pointer in a1 */
     make_temp_string();
-    gen("lea",tempstrname,"a0");  /* unique temp holder */
+    gen_load_addr(tempstrname,0);  /* unique temp holder */
 
     /* when storing an input value into an array element, must save
        value (d0) first, since array index calculation may be corrupted

src/ACE/C/memory.c
@@ -229,7 +229,7 @@ void swap()
 
       /* copy first to temp */
       gen("movea.l","d1","a1");    /* source */ 
-      gen("lea",tempstrname,"a0"); /* dest */ 
+      gen_load_addr(tempstrname,0); /* dest */ 
       gen_jsr("_strcpy");   /* temp = first */
 
       /* copy second to first */
@@ -238,7 +238,7 @@ void swap()
       gen_jsr("_strcpy");   /* first = second */ 
 
       /* copy temp to second */
-      gen("lea",tempstrname,"a1"); /* source */ 
+      gen_load_addr(tempstrname,1); /* source */ 
       gen("movea.l","d2","a0");    /* dest */
       gen_jsr("_strcpy");   /* second = temp */
 

commit 45f158138c1f75e9c5071e80e63467a5a10e734e

Fixed regression that first occurred in 30c3dcb

src/ACE/C/assign.c
@@ -627,7 +627,7 @@ void input() {
 	  
 	  /* ALL data types need a temporary string */
 	  make_temp_string();
-	  if (storage->type != stringtype) gen_load_addr(tempstrname,0);
+	  if (storage->type != stringtype) gen_load_addr(tempstrname,1);
 	  else gen_pea(tempstrname);
 	  
 	  /* When storing an input value into an array element, must save